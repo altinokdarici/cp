9b7e405f4de163f4f9e058b667c417c48d2ceb7f
github-action-benchmark <github@users.noreply.github.com>
2026-03-06 20:11:03 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4422bfe31e0c089932096975f227ae743dc07693

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772826946802,
+  "lastUpdate": 1772827863367,
   "repoUrl": "https://github.com/altinokdarici/cp",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "large (500 modules, 10 entries)",
             "value": 26348554,
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "altinokd@outlook.com",
+            "name": "Altinok Darici",
+            "username": "altinokdarici"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4422bfe31e0c089932096975f227ae743dc07693",
+          "message": "fix: support circular dependencies in module graph (#1)",
+          "timestamp": "2026-03-06T12:10:30-08:00",
+          "tree_id": "cb8b3be5db7149018c0d175f8205667afac360e1",
+          "url": "https://github.com/altinokdarici/cp/commit/4422bfe31e0c089932096975f227ae743dc07693"
+        },
+        "date": 1772827862631,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "small (20 modules, 2 entries)",
+            "value": 1237445,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "medium (100 modules, 5 entries)",
+            "value": 5423423,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "large (500 modules, 10 entries)",
+            "value": 26297156,
             "unit": "ns/iter"
           }
         ]
